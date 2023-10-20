e66dc4ec3c778c5487d8fa309db49b7bbc019f5a
Kween <kweenyoallhiada@gmail.com>
2023-10-20 00:08:58 -0700

    add completed file

---
+++ addTwo.js
@@ -0,0 +1,12 @@
+/*Challenge 1
+Create a function addTwo that accepts one input and adds 2 to it.
+*/
+function addTwo(num) {
+
+    return num + 2;
+ 
+ }
+ 
+ // To check if you've completed it, uncomment these console.logs!
+ console.log(addTwo(3));
+ console.log(addTwo(10));
