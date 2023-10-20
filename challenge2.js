0f22126c182ce7e52c5c75bf24c59dc03404fe4a
Kween <kweenyoallhiada@gmail.com>
2023-10-20 00:12:15 -0700

    add complete file

---
+++ challenge2.js
@@ -0,0 +1,14 @@
+/*Challenge 2
+Create a function addS that accepts one input and adds an "s" to it.
+ */
+
+function addS(word) {
+  
+  return word + 's';
+  
+  }
+  
+  // uncomment these to check your work
+  console.log(addS('pizza'));
+  console.log(addS('bagel'));
+  
